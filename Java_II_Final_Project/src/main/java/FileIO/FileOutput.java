d86122d55eca5ab48ea98e6b699427e36f0a1b07
troy84baby <132592361+troy84baby@users.noreply.github.com>
2023-05-04 11:23:01 -0400

    Update FileOutput.java
    Created file and database

--- Java_II_Final_Project/src/main/java/FileIO/FileOutput.java
+++ Java_II_Final_Project/src/main/java/FileIO/FileOutput.java
@@ -1,13 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package FileIO;
-
 /**
  *
- * @author Jon
+ * @author Troy Harrison
  */
+package FileIO;
+import java.util.ArrayList;
+import java.sql.*;
+import java.io.*;
+import Classes.Pitcher; 
+
 public class FileOutput {
-    
+    public static String Output(String filename, ArrayList <Pitcher> names){
+        // open an output stream 
+        try (
+            PrintWriter out = new PrintWriter(
+                          new BufferedWriter(
+                          new FileWriter(filename)))) {
+            out.close();
+        } catch (Exception E){
+            return E.getMessage();
+        } 
+        String URL = "jdbc:sqlite:" + filename;
+        try (
+            Connection connection = DriverManager.getConnection(URL)){
+            String pitcherDatabase = "CREATE TABLE StatsTable("
+                    + "LastName TEXT    ,"
+                    + "FirstName TEXT   ,"
+                    + "TeamName TEXT    ,"
+                    + "ip   REAL        ,"
+                    + "ab   INTEGER     ,"
+                    + "h    INTEGER     ,"
+                    + "r    INTEGER     ,"
+                    + "er   INTEGER     ,"
+                    + "w    INTEGER     ,"
+                    + "so   INTEGER     ,"
+                    + "tbf  INTEGER     ,"
+                    + "np   INTEGER     "
+                    + ")";
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(pitcherDatabase);
+            for (int i = 0; i < names.size(); i++) {
+                Pitcher pitcher = names.get(i);
+                String insert = "INSERT INTO StatsTable VALUES ("
+                        + "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                PreparedStatement ps = connection.prepareStatement(insert);
+                ps.setString(1, pitcher.getLastName());
+                ps.setString(2, pitcher.getFirstName());
+                ps.setString(3, pitcher.getTeamName());
+                ps.setString(4, pitcher.getInningsPitched()+"");
+                ps.setString(5, pitcher.getAtBats()+"");
+                ps.setString(6, pitcher.getHits()+"");
+                ps.setString(7, pitcher.getRunScored()+"");
+                ps.setString(8, pitcher.getEarnedRuns()+"");
+                ps.setString(9, pitcher.getWalks()+"");
+                ps.setString(10, pitcher.getStrikeouts()+"");
+                ps.setString(11, pitcher.getBattersFaced()+"");
+                ps.setString(12, pitcher.getNopitches()+"");
+                ResultSet finishedTable = ps.executeQuery();
+            }
+            return "";
+        } catch (SQLException E) {
+            return E.getMessage();
+        }               
+    }
 }
+
+
